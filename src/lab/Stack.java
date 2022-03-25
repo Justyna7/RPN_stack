16ae2e37542156751fb0bcad34f8b7fda43ead0e
Justyna <justiluc@interia.pl>
2022-03-25 12:14:28 +0100

    testy do stack test

---
+++ src/lab/Stack.java
@@ -0,0 +1,32 @@
+/*
+Zaimplementuj klasę Stack implementującą ideę stosów napisów z następującymi metodami publicznymi:
+
+* push wkłada jeden element na stos
+* pop zdejmuje jeden element ze stosu i oddaje wartość tego elementu;
+co się ma dziać gdy, pop próbuje zdjąc element z pustego stosu?
+* peek podobnie jak pop oddaje wartość elementu na szczycie stosu ale go nie zdejmuje;
+podobny problem z pustym stosem co w przypadku pop.
+Podstawową strukturą danych w implementacji stosu powinna być tablica.
+Stos nie powinien posiadać ograniczeń rozmiaru.
+ */
+
+package lab;
+
+import java.util.List;
+import java.lang.String;
+
+public class Stack {
+    private String stos[] = new String[0];
+    public void  push(String s){
+
+    }
+    public String pop(){
+        return "";
+    }
+    public String peek(){
+        return "";
+    }
+    public int len(){
+        return 0;
+    }
+}
---
+++ src/lab/StackTest.java
@@ -0,0 +1,59 @@
+/* Przygotuj testy jednostkowe dla klasy Stack. */
+
+package lab;
+import org.junit.*; //
+import static org.junit.Assert.assertEquals;//
+
+public class StackTest {
+    @Test
+    public void testEmpty() {
+        // Arrange
+        // sut = System Under Test
+        Stack sut = new Stack();
+        // Act
+        // Assert
+        assertEquals("sut.len()", 0, sut.len());
+    }
+    @Test
+    public void testAddOne() {
+        // Arrange
+        // sut = System Under Test
+        Stack sut = new Stack();
+        // Act
+        sut.push("ala");
+        // Assert
+        assertEquals("sut.push(\"ala\")", "ala", sut.peek());
+    }
+    @Test
+    public void testAddOne2() {
+        // Arrange
+        // sut = System Under Test
+        Stack sut = new Stack();
+        // Act
+        sut.push("ala");
+        // Assert
+        assertEquals("sut.push(\"ala\")", 1, sut.len());
+    }
+    @Test
+    public void testTakeOne() {
+        // Arrange
+        // sut = System Under Test
+        Stack sut = new Stack();
+        // Act
+        sut.push("ala");
+        // Assert
+        assertEquals("sut.pop()", "ala", sut.pop());
+    }
+    @Test
+    public void testTakeOne2() {
+        // Arrange
+        // sut = System Under Test
+        Stack sut = new Stack();
+        // Act
+        sut.push("ala");
+        sut.push("xxxx");
+        sut.pop();
+        // Assert
+        assertEquals("sut.pop()", 1, sut.len());
+    }
+}
