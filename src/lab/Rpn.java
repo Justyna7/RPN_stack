f1101186c4a07718147881fbb71159bc737f6337
Justyna <justiluc@interia.pl>
2022-04-05 21:09:45 +0200

    rpm - testadd1and3 przechodzi

--- src/lab/Rpn.java
+++ src/lab/Rpn.java
@@ -13,21 +13,40 @@ public class Rpn {
     }
     public int compute(){
         int l = this.s.length();
-        char a = '0';
-        char b = '1';
-        char c = '9';
-        int aa = a;
-        int bb = b;
-        int cc = c;
-        System.out.println(""+ aa+ " "+ bb+ " " + cc + " ");
+//        char a = '0';
+//        char b = '1';
+//        char c = '9';
+//        int aa = a;
+//        int bb = b;
+//        int cc = c;
+       // System.out.println(""+ aa+ " "+ bb+ " " + cc + " ");
+        int l2 = 0;
         for (int i = 0; i<l; i++){
             String s1 = s.substring(i, i+1);
             //System.out.println(""+ i + " " + s.charAt(i)+ " " + (!s1.equals(" ")));
             if (!s1.equals(" ")){
                 this.stos.push(s1);
+                l2 = l2 +1;
             }
-
         System.out.println(""+ this.s + " " + this.stos.peek());
+        }
+        boolean t = true;
+        while (t){
+            String s1 = this.stos.pop();
+            switch (s1){
+                case "+":
+                    int a = Character.getNumericValue(this.stos.pop().charAt(0));
+                    int b = Character.getNumericValue(this.stos.pop().charAt(0));
+                    //System.out.println(""+ a + " " + b+ " " + (a+b));
+                    this.stos.push(""+(a + b));
+                    System.out.println(""+ a + " " + b+ " " + (a+b) + " " + this.stos.peek()+ " "+ Character.getNumericValue(this.stos.peek().charAt(0)));
+                    t = false;
+                    break;
+                default:
+                    this.stos.push(s1);
+                    t = false;
+            }
+            //System.out.println(""+ this.s + " " + this.stos.peek());
         }
         return Character.getNumericValue(this.stos.peek().charAt(0));
     }
